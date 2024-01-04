642ba5ea062a6c48b856cfa94f0f5327c6b6b9e0
giannismon <mongiannis@gmail.com>
2024-01-04 23:39:31 +0200

    --

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -11,16 +11,6 @@ pipeline {
             }
         }
 
-        stage('Deploy to Tomcat') {
-            agent {
-                label 'dev'
-            }
-            steps {
-                script {
-                    // Κάνε την απαραίτητη ενέργεια για να αντιγράψεις το αρχείο στον Tomcat
-                    sh "cp target/*.war /path/to/tomcat/webapps/"
-                }
-            }
-        }
+
     }
 }
