5ba029fc97e350d238e3f796e101514823bbf058
giannismon <mongiannis@gmail.com>
2024-01-05 19:23:18 +0200

    --

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -11,6 +11,7 @@ pipeline {
                 label 'master'
             }
             steps {
+
                 sh 'mvn clean install'
                 sh 'hostname'
                 sh 'pwd'
@@ -23,18 +24,16 @@ pipeline {
         }
 
 
-        stage('tomcat') {
+
+        stage('Deploy tomcat') {
             agent {
                 label 'dev'
             }
-
             steps {
                 sh 'hostname'
                 sh 'pwd'
-                sh 'rm -rf *'
+                cleanWs()
                 sh 'ls'
-
-
                 unstash 'buildResults'
                 sh 'ls target'
                 sh 'mv **/*.war /root/apache-tomcat-9.0.70/webapps/'
@@ -43,8 +42,42 @@ pipeline {
         }
 
 
+        stage('Restart tomcat') {
+            agent {
+                label 'dev'
+            }
+            steps {
+                sh '/root/apache-tomcat-9.0.70/bin/shutdown.sh'
+                sh 'sleep 5' // Περιμένει 5 δευτερόλεπτα
+                sh '/root/apache-tomcat-9.0.70/bin/startup.sh'
+            }
+        }
 
 
+        stage('Check Tomcat Status') {
+            agent {
+                label 'dev'
+            }
+            steps {
+                script {
+                    def tomcatProcess = sh(script: 'ps aux | grep "[t]omcat"', returnStatus: true)
+
+                    if (tomcatProcess == 0) {
+                        echo 'Tomcat process found. Checking if it is actually Tomcat...'
+
+                        // Check if the process is really Tomcat
+                        def isTomcat = sh(script: 'ps aux | grep "[t]omcat" | grep "catalina.base"', returnStatus: true)
+
+                        if (isTomcat == 0) {
+                            echo 'Tomcat is running.'
+                        } else {
+                            error 'Process found, but it may not be Tomcat.'
+                        }
+                    } else {
+                        error 'Tomcat process not found.'
+                    }
+            }
+        }
 
 
 
