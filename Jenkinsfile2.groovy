5d82bd3c7a8e319073476b926f86dc025e4f2372
giannismon <mongiannis@gmail.com>
2024-01-05 15:57:25 +0200

    --

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -11,13 +11,32 @@ pipeline {
                 label 'master'
             }
             steps {
-                sh 'mvn clean install'
+                //sh 'mvn clean install'
                 sh 'pwd'
+                sh 'ls'
                 sh "echo '##########################################################'"
 
                 stash includes : "*", name : "buildResults"
             }
         }
+
+
+
+        stage('tomcat') {
+            agent {
+                label 'dev'
+            }
+            steps {
+                sh 'pwd'
+                sh 'ls'
+                stash includes : "*", name : "buildResults"
+            }
+        }
+
+
+
+
+
 
 
     }
