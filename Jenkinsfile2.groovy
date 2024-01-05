56b4cbcce7e80c0692103af4777faa5590ef551a
giannismon <mongiannis@gmail.com>
2024-01-05 14:09:50 +0200

    --

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -17,13 +17,16 @@ pipeline {
 
 
 
-    stage('Transfer to Node with Tomcat 9') {
-        steps {
-            script {
-                // Εκτελεί την εντολή SCP για τη μεταφορά του αρχείου .war
-                sh "scp /var/lib/jenkins/workspace/git/target/helloworld.war root@$192.168.1.10:/root/tomcat/webapps/"
+        stage('Transfer to Node with Tomcat 9') {
+            agent {
+                label 'master'
+            }
+            steps {
+                script {
+                    // Εκτελεί την εντολή SCP για τη μεταφορά του αρχείου .war
+                    sh "scp /var/lib/jenkins/workspace/git/target/helloworld.war root@$192.168.1.10:/root/tomcat/webapps/"
+                }
             }
         }
     }
-
 }
