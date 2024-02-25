22a4552f702b23b4a786ded893ed8c7f2b6848c1
giannismon <mongiannis@gmail.com>
2024-02-25 23:37:50 +0200

    ---------------

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -41,17 +41,6 @@ pipeline {
 
 
         
-        stage("Check out") {
-            agent {
-                label 'master'
-            }
-            steps {
-                script {
-                    git branch: 'master', url: 'https://github.com/giannismon/tomcat-application-main.git';
-                }
-            }
-        }
-
 
 
 
