db64414086d10560f2689c7db452dab6603bf1b2
giannismon <mongiannis@gmail.com>
2024-02-25 23:46:12 +0200

    ---------------

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -5,56 +5,7 @@ pipeline {
         maven "Maven"
     }
 
-
-    environment {
-        // This can be nexus3 or nexus2
-        NEXUS_VERSION = "nexus3"
-        // This can be http or https
-        NEXUS_PROTOCOL = "http"
-        // Where your Nexus is running
-        NEXUS_URL = "192.168.1.99:8081"
-        // Repository where we will upload the artifact
-        NEXUS_REPOSITORY = "LoginWebApp"
-        // Jenkins credential id to authenticate to Nexus OSS
-        NEXUS_CREDENTIAL_ID = "nexus"
-        ARTIFACT_VERSION = "${BUILD_NUMBER}"
-    }
-
-
-
-
-
     stages {
-
-        stage('Verify Environment') {
-            steps {
-                script {
-                    echo "NEXUS_VERSION: ${NEXUS_VERSION}"
-                    echo "NEXUS_PROTOCOL: ${NEXUS_PROTOCOL}"
-                    echo "NEXUS_URL: ${NEXUS_URL}"
-                    echo "NEXUS_REPOSITORY: ${NEXUS_REPOSITORY}"
-                    echo "NEXUS_CREDENTIAL_ID: ${NEXUS_CREDENTIAL_ID}"
-                    echo "ARTIFACT_VERSION: ${ARTIFACT_VERSION}"
-                }
-            }
-        }
-
-
-        
-        stage("Check out") {
-            agent {
-                label 'master'
-            }
-            steps {
-                script {
-                    git branch: 'master', url: 'https://github.com/giannismon/tomcat-application-main.git';
-                }
-            }
-        }
-
-
-
-
         stage('Build') {
             agent {
                 label 'master'
@@ -71,54 +22,6 @@ pipeline {
                 stash includes: "target/**", name: "buildResults"
             }
         }
-
-
-
-        stage("publish to nexus") {
-            agent {
-                label 'master'
-            }
-            steps {
-                script {
-                    sh "echo 'UPLOADDDDDDDDDDDDDDDDDDDDDDDDDD ##########################################################'"
-                    // Read POM xml file using 'readMavenPom' step , this step 'readMavenPom' is included in: https://plugins.jenkins.io/pipeline-utility-steps
-                    pom = readMavenPom file: "pom.xml";
-                    // Find built artifact under target folder
-                    filesByGlob = findFiles(glob: "target/*.${pom.packaging}");
-                    // Print some info from the artifact found
-                    echo "${filesByGlob[0].name} ${filesByGlob[0].path} ${filesByGlob[0].directory} ${filesByGlob[0].length} ${filesByGlob[0].lastModified}"
-                    // Extract the path from the File found
-                    artifactPath = filesByGlob[0].path;
-                    // Assign to a boolean response verifying If the artifact name exists
-                    artifactExists = fileExists artifactPath;
-
-                    if(artifactExists) {
-                        echo "*** File: ${artifactPath}, group: ${pom.groupId}, packaging: ${pom.packaging}, version ${pom.version}";
-
-                        nexusArtifactUploader(
-                            nexusVersion: NEXUS_VERSION,
-                            protocol: NEXUS_PROTOCOL,
-                            nexusUrl: NEXUS_URL,
-                            groupId: pom.groupId,
-                            version: ARTIFACT_VERSION,
-                            repository: NEXUS_REPOSITORY,
-                            credentialsId: NEXUS_CREDENTIAL_ID,
-                            artifacts: [
-                                // Artifact generated such as .jar, .ear and .war files.
-                                [artifactId: pom.artifactId,
-                                classifier: '',
-                                file: artifactPath,
-                                type: pom.packaging]
-                            ]
-                        );
-
-                    } else {
-                        error "*** File: ${artifactPath}, could not be found";
-                    }
-                }
-            }
-        }
-
 
 
 
@@ -191,6 +94,23 @@ pipeline {
 
 
 
+
+        stage('Ping URL') {
+            agent {
+                label 'dev'
+            }
+            steps {
+                script {
+                    def response = sh(script: "wget --spider -S http://192.168.1.8:8080/helloworld/ 2>&1 | grep 'HTTP/1.1 200'", returnStatus: true)
+
+                    if (response == 0) {
+                        echo "Ping successful."
+                    } else {
+                        error "Failed to ping."
+                    }
+                }
+            }
+        }
 
     }
 
