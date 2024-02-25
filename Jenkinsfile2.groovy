703e0090b13abd311c41ef06b26ac041cb6132c1
giannismon <mongiannis@gmail.com>
2024-02-25 22:58:18 +0200

    ---------------

--- Jenkinsfile2.groovy
+++ Jenkinsfile2.groovy
@@ -5,6 +5,23 @@ pipeline {
         maven "Maven"
     }
 
+
+    environment {
+        // This can be nexus3 or nexus2
+        NEXUS_VERSION = "nexus3"
+        // This can be http or https
+        NEXUS_PROTOCOL = "http"
+        // Where your Nexus is running
+        NEXUS_URL = "192.168.1.99:8081"
+        // Repository where we will upload the artifact
+        NEXUS_REPOSITORY = "LoginWebApp"
+        // Jenkins credential id to authenticate to Nexus OSS
+        NEXUS_CREDENTIAL_ID = "nexus"
+        ARTIFACT_VERSION = "${BUILD_NUMBER}"
+    }
+
+
+
     stages {
         stage('Build') {
             agent {
